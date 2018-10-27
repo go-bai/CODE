7dbb5ece1bfb8c4978bb2ac59ab23476eea5199e
libo <38973236+libailibai@users.noreply.github.com>
2018-10-27 23:58:27 +0800

    Update poj3299.cpp

--- POJ刷题/poj3299.cpp
+++ POJ刷题/poj3299.cpp
@@ -12,38 +12,18 @@ int main() {
         cin>>c;
         if(c=='E') break;
         switch(c) {
-        case 'T':
-            tt=false;
-            cin>>t;
-            break;
-        case 'D':
-            dd=false;
-            cin>>d;
-            break;
-        case 'H':
-            hh=false;
-            cin>>h;
-            break;
+        case 'T':tt=false;cin>>t;break;
+        case 'D':dd=false;cin>>d;break;
+        case 'H':hh=false;cin>>h;break;
         }
         cin>>c;
         switch(c) {
-        case 'T':
-            tt=false;
-            cin>>t;
-            break;
-        case 'D':
-            dd=false;
-            cin>>d;
-            break;
-        case 'H':
-            hh=false;
-            cin>>h;
-            break;
+        case 'T':tt=false;cin>>t;break;
+        case 'D':dd=false;cin>>d;break;
+        case 'H':hh=false;cin>>h;break;
         }
-        if(tt)
-            t=h-0.5555*(6.11*exp(5417.7530*((1/273.16)-(1/(d+273.16))))-10.0);
-        else if(dd)
-            d=1/(1/273.16-log(((h-t)/0.5555+10)/6.11)/5417.7530)-273.16;
+        if(tt) t=h-0.5555*(6.11*exp(5417.7530*((1/273.16)-(1/(d+273.16))))-10.0);
+        else if(dd) d=1/(1/273.16-log(((h-t)/0.5555+10)/6.11)/5417.7530)-273.16;
         else h=t+0.5555*(6.11*exp(5417.7530*((1/273.16)-(1/(d+273.16))))-10.0);
         cout<<"T "<<t<<" D "<<d<<" H "<<h<<endl;
     }
